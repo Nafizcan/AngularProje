3644b5d7f55ef2b8d35642666e1cf039cf4198bd
Nafizcan Engin <119319570+Nafizcan@users.noreply.github.com>
2023-01-12 19:30:43 +0300

    Final Projesi
    İnternet Programcılığı Dersi Final Projesi

---
+++ sorucevap-portali/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'sorucevap-portali',
+    appId: '1:915074241446:web:88c854054d5dee2416f567',
+    storageBucket: 'sorucevap-portali.appspot.com',
+    apiKey: 'AIzaSyDmI8vIdGS8jFAwKdv3LmvI77RSanzcqrY',
+    authDomain: 'sorucevap-portali.firebaseapp.com',
+    messagingSenderId: '915074241446',
+  },
+  production: true
+};
---
+++ sorucevap-portali/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'sorucevap-portali',
+    appId: '1:915074241446:web:88c854054d5dee2416f567',
+    storageBucket: 'sorucevap-portali.appspot.com',
+    apiKey: 'AIzaSyDmI8vIdGS8jFAwKdv3LmvI77RSanzcqrY',
+    authDomain: 'sorucevap-portali.firebaseapp.com',
+    messagingSenderId: '915074241446',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
